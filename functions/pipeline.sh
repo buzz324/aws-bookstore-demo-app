d3d8648f582a42960d9046dd42f16defd9d6e2f6
buzz324 <ejkho24@gmail.com>
2021-05-31 22:55:05 +1200

    COMPX341A1V3

--- functions/pipeline.sh
+++ functions/pipeline.sh
@@ -1,10 +1,11 @@
 
 #npm install
-npm run build #build application
-cd src/test
-jest #form a test within our test folder
-cd ../../../ #go back to main directory
-git add . #add new changed file to git repo,staging area
-git commit -m $1 #commit with message, $1 is first argument that is passed to the pipeline (to local )
+
+npm run build 
+cd src
+jest 
+cd ../../../ 
+git add .
+git commit -m $1 
 git push origin master
 git status
