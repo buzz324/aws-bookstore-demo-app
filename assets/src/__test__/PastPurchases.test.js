929d6895b8dca83249607c0e203fe333c43e5cf4
buzz324 <ejkho24@gmail.com>
2021-06-04 18:38:56 +1200

    lastcommit_question2c

--- assets/src/__test__/PastPurchases.test.js
+++ assets/src/__test__/PastPurchases.test.js
@@ -1,26 +1,60 @@
-import {jest} from '@jest/globals'
-
 import PastPurchases from "../modules/pastPurchases/PastPurchases";
 import { Auth, API } from "aws-amplify";
+import { userInfo } from "os";
+import supertest from "supertest";
 
 describe('Check componentDidMount function', () => {
 
     const mockPastPurchase = jest.fn().mockReturnThis();
     const pastPurchases = new PastPurchases(mockPastPurchase)
 
-    console.log(pastPurchases);
-
-    const mockUserInfo = jest.fn(Auth.currentUserInfo()).mockImplementation(x=>{return 1;})
+    const userInfo = jest.fn(Auth.currentUserInfo()).mockImplementation(x=>{return 1;})
+    const orders= [
+        {
+            orderDate: '2021-10-29',
+            orderId: '1234',
+            books: [ {bookId: '12345',
+                quantity: 12,
+                price: 12}]
+        }
+    ];
     PastPurchases.componentDidMount()
 
+    //Testing componentDidMount 
+
     it('test AWS Authentication', ()=> {
+        //Recevie default value of Auth.currentUserInfor using moking function 
+
         Auth.currentUserInfo();
-        expect(mockUserInfo).toHaveBeenCalledTimes(1);
+        //output of number of callings are expected from the test
+        expect(userInfo).toHaveBeenCalledTimes(1);
+
+
     });
 
-    it('check in BestSellerProductRow.tsx', ()=> {
-        expect(checkId&&checkName.test(jsFiles)).toBeTruthy();
+    //Check if the order API is received to local host
+  it('Test GET /order', ()=> {
+                
+         supertest("http://localhost:3000")
+        .get('/order')
+        .expect('Content-Type', /json/)
+        .expect('Content-Length', '15')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) throw err;
+
+          //Set default conditions for order variables to test if they receive their default values
+          expect(res.body.orderDate).toBe("2021-10-29-")
+          expect(res.body.orderId).toBe("1234")
+          expect(res.body.orderId.bookId).toBe("12345")
+          expect(res.body.orderId.quantity).toBe("12")
+          expect(res.body.orderId.price).toBe("12")
+        
+        });
+
     });
+    
+
    
   });
   
